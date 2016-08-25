0530b3e4daf4f6711ba8508a83f3d8ef7d73a4cf
saferoom-app <contact@getsaferoom.com>
2016-08-25 12:46:14 +0200

    Config commit

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ ENCRYPTED_PREFIX = "TUFNTU9USEVOQ1JZUFRFRE5PVEU=__"
 ENCRYPTED_SUFFIX = "__TUFNTU9USEVOQ1JZUFRFRE5PVEU="
 
 # HTML templates
-html_attach = "<div class=\"attachment\"><div class=\"row\"><div class=\"col-md-10\" style=\"display:inline-block\"><span style=\"margin-left:10px\"><img src=\"/static/images/::fileicon::\"/></span><span style=\"margin-left:10px\"><a href=\"static/tmp/::filename::\">::filename::</a></span></div><div class=\"col-md-2\"></div></div></div><br/>"
+html_attach = "<div class=\"row\"><div class=\"col-md-10\" style=\"display:inline-block\"><span style=\"margin-left:10px\"><img src=\"/static/images/::fileicon::\"/></span><span style=\"margin-left:10px\"><a href=\"static/tmp/::filename::\">::filename::</a></span></div><div class=\"col-md-2\"></div></div>"
 
 # Success messages
 MSG_NOTECREATE_OK = "Note has been successfully created and uploaded to the server"
@@ -27,4 +27,17 @@ TYPE_JSON = "json"
 TYPE_TABLE = "table"
 TYPE_SELECT = "select"
 TYPE_LIST = "list"
+
+# Supported MIME types
+MIME_PDF = "application/pdf"
+MIME_DOC = "application/msword"
+MIME_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+MIME_XLS = "application/vnd.ms-excel"
+MIME_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+MIME_PPT = "application/vnd.ms-powerpoint"
+MIME_PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+MIME_JPEG = "image/jpeg"
+MIME_JPG = "image/jpg"
+MIME_GIF = "image/gif"
+MIME_PNG = "image/png"
 
