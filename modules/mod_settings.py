6ec292a0e9cdf993ed3373827a7a7b47d18825d4
saferoom-app <contact@getsaferoom.com>
2016-08-31 17:02:17 +0200

    Commit

--- modules/mod_settings.py
+++ modules/mod_settings.py
@@ -12,7 +12,4 @@ mod_settings = Blueprint("mod_settings",__name__)
 # Routes
 @mod_settings.route("/",methods=['GET'])
 def show_page():
-    encrypted_password = encryptString("testtest",getpass.getuser()+"__"+config.salt)
-    print encrypted_password
-    print decryptString(encrypted_password,getpass.getuser()+"__"+config.salt)
     return render_template("settings.html",title="Settings")
